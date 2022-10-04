acc33fbfc6ab6c2ee94a64089b0f484b661e7118
Danil Murzin <danil.murzin.200026@gmail.com>
2022-10-04 17:51:35 +0300

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import logoImg from '../image/logo.svg';
+import signImg from '../image/sign.svg';
+
+const NavBarStyled = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  min-height: 80px;
+  background-color: #299b01;
+  z-index: 999;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 30px;
+`;
+
+const Logo = styled.div`
+  display: flex;
+`;
+
+const H1 = styled.h1`
+  font-size: 24px;
+  margin-left: 15px;
+`;
+
+const ImgLogo = styled.img`
+  width: 50px;
+`;
+
+const SignBtn = styled.button`
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+`;
+
+export const NavBar = () => (
+  <NavBarStyled>
+    <Container>
+      <Logo>
+        <ImgLogo src={logoImg} alt="logo" />
+        <H1>MrDonald's</H1>
+      </Logo>
+      <SignBtn>
+        Войти
+        <img src={signImg} alt="sign" />
+      </SignBtn>
+    </Container>
+  </NavBarStyled>
+);
