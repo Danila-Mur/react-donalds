e85c42fee68afa5f6c0960fa2c49accb7ca908d3
Danil Murzin <danil.murzin.200026@gmail.com>
2022-10-20 21:33:37 +0300

    add firebase db

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import dbMenu from '../DBMenu';
 import { ListItem } from './ListItem';
 import { Banner } from './Banner';
+import { useFetch } from '../Hooks/useFetch';
 
 const MenuStyled = styled.main`
   background-color: #ccc;
@@ -14,16 +14,103 @@ const SectionMenu = styled.section`
   padding 30px;
 `;
 
-export const Menu = ({ setOpenItem }) => (
-  <MenuStyled>
-    <Banner />
-    <SectionMenu>
-      <h2>Бургеры</h2>
-      <ListItem itemList={dbMenu.burger} setOpenItem={setOpenItem} />
-    </SectionMenu>
-    <SectionMenu>
-      <h2>Закуски / Напитки</h2>
-      <ListItem itemList={dbMenu.other} setOpenItem={setOpenItem} />
-    </SectionMenu>
-  </MenuStyled>
-);
+const Preloader = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+const Loader = styled.div`
+  display: block;
+  position: relative;
+  left: 50%;
+  top: 50%;
+  width: 150px;
+  height: 150px;
+  margin: -75px 0 0 -75px;
+  border-radius: 50%;
+  border: 3px solid transparent;
+  border-top-color: #9370db;
+  -webkit-animation: spin 2s linear infinite;
+  animation: spin 2s linear infinite;
+  &:after {
+    content: '';
+    position: absolute;
+    top: 15px;
+    left: 15px;
+    right: 15px;
+    bottom: 15px;
+    border-radius: 50%;
+    border: 3px solid transparent;
+    border-top-color: #ff00ff;
+    -webkit-animation: spin 1.5s linear infinite;
+    animation: spin 1.5s linear infinite;
+  }
+  &:before {
+    content: '';
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    right: 5px;
+    bottom: 5px;
+    border-radius: 50%;
+    border: 3px solid transparent;
+    border-top-color: #ba55d3;
+    -webkit-animation: spin 3s linear infinite;
+    animation: spin 3s linear infinite;
+  }
+  @-webkit-keyframes spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      -ms-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      -ms-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+export const Menu = ({ setOpenItem }) => {
+  const res = useFetch();
+  const dbMenu = res.response;
+
+  return (
+    <MenuStyled>
+      <Banner />
+      {res.response ? (
+        <>
+          <SectionMenu>
+            <h2>Бургеры</h2>
+            <ListItem itemList={dbMenu.burger} setOpenItem={setOpenItem} />
+          </SectionMenu>
+          <SectionMenu>
+            <h2>Закуски / Напитки</h2>
+            <ListItem itemList={dbMenu.other} setOpenItem={setOpenItem} />
+          </SectionMenu>
+        </>
+      ) : res.error ? (
+        <div>Sorry, we will fixed it soon...</div>
+      ) : (
+        <Preloader>
+          <Loader></Loader>
+        </Preloader>
+      )}
+    </MenuStyled>
+  );
+};
